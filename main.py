2be9eea9548504fdaa78d9f01def10f375cc3dee
JorgeDh <herrerajorge725@gmail.com>
2024-03-24 00:16:55 -0600

    deployment 3

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def db_connection():
     return users
 app = FastAPI()
 
-#deployment2
+#deployment3
 @app.post("/create")
 async def create(user: User):
     if user.mail != "" and user.name != "" and user.password != "":
